61db3d3077e8068662b1d54f31d70d604f2c7c3a
Justin Xue <43502772+shoexue@users.noreply.github.com>
2024-07-29 11:38:52 -0400

    add to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div class='mt-12 w-max-[800px] border-t border-gray-500 text-center'>
         <p className='my-5 text-gray-500'>
-            Made with <FaHeart className='inline-block text-red-500' /> <br />(Made with React)
+            Made with <FaHeart className='inline-block text-red-500' /> <br />(Made with React + Tailwind)
         </p>
         <div class="inline-flex text-gray-500 gap-4 text-3xl">
             <FaReact/>
